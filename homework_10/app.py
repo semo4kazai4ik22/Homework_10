9d2ec3ab8a599632537f4bd346b4ac029ccaa5a5
Legion <semo4kazai4ik@yandex.ru>
2022-06-27 00:52:43 +0700

    App - add profile_page, skills_page, fix main_page Functions - add new functions (format_candidates, get_uid, get_all_candidates, get_candidate_by_skill) fix json

--- homework_10/app.py
+++ homework_10/app.py
@@ -1,4 +1,4 @@
-from functions import load_data
+from functions import load_data, format_candidates, get_uid, get_all_candidates, get_candidate_by_skill
 
 from flask import Flask
 
@@ -8,26 +8,29 @@ filename = "candidates.json"
 
 @app.route('/')
 def main_page():
-    candidates: list[dict] = load_data(filename)
-    candidate = "<pre>"
+    """Вывод кандидатов на главную"""
+    candidates: list[dict] = get_all_candidates()
+    result: str = format_candidates(candidates)
+    return result
 
-    for candidate_ in candidates:
-        candidate += f"{candidate_['name']}\n{candidate_['position']}\n{candidate_['skills']}"
-    candidate += "</pre>"
-    return candidate
-# <pre>
-#   Имя кандидата -
-#   Позиция кандидата
-#   Навыки через запятую
-#
-#   Имя кандидата -
-#   Позиция кандидата
-#   Навыки через запятую
-#
-#   Имя кандидата -
-#   Позиция кандидата
-#   Навыки через запятую
-# <pre>
+
+@app.route('/candidates/<int:uid>')
+def profile_page(uid):
+    """Поиск кандидата по id"""
+    candidate: dict = get_uid(uid)
+    result = f'<img src="{candidate["picture"]}">'
+    result += format_candidates([candidate])
+    return result
+
+
+@app.route('/skills/<skill>')
+def skills_page(skill):
+    """Поиск кандидата по навыку"""
+    candidates: list[dict] = get_candidate_by_skill(skill)
+    result = format_candidates(candidates)
+    return result
+
+
 
 if __name__ == "__main__":
     app.run()
--- homework_10/functions.py
+++ homework_10/functions.py
@@ -1,6 +1,40 @@
 import json
 
+filename = "candidates.json"
+
 def load_data(filename) -> list[dict]:
     with open(filename, 'r', encoding="utf-8") as f:
         data = json.load(f)
         return data
+
+
+def format_candidates(candidates: list[dict]) -> str:
+
+    candidate = "<pre>"
+
+    for candidate_ in candidates:
+        candidate += f"{candidate_['name']}\n"
+        candidate += f"{candidate_['position']}\n"
+        candidate += f"{candidate_['skills']}\n\n"
+    candidate += "</pre>"
+    return candidate
+
+def get_all_candidates() -> list[dict]:
+    return load_data(filename)
+
+
+def get_uid(uid: int) -> dict | None:
+    candidates = get_all_candidates()
+
+    for candidate in candidates:
+        if candidate["id"] == uid:
+            return candidate
+    return None
+
+def get_candidate_by_skill(skill: str) -> list[dict]:
+    candidates = get_all_candidates()
+    result = []
+    for candidate in candidates:
+        if skill in candidate["skills"].lower().split(", "):
+            result.append(candidate)
+    return result
